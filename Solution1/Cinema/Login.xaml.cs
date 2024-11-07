d196d691347b13685568a63199d40479b73df768
Nguyen Minh Hung <Hackinghungtx@gmail.com>
2024-11-07 17:23:58 +0700

    login

---
+++ Solution1/Cinema/Login.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Cinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cinema
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        private CinemaContext _context;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            using (var context = new CinemaContext())
+            {
+                var user = context.Users.FirstOrDefault(u => u.Account == username && u.Password == password);
+
+                if (user != null)
+                {
+                    MessageBox.Show($"Welcome {user.Role}!");
+                    // Nếu đăng nhập thành công, bạn có thể mở một cửa sổ khác hoặc chuyển hướng.
+                }
+                else
+                {
+                    ErrorText.Text = "Invalid username or password.";
+                }
+            }
+        }
+    }
+}
---
+++ Solution1/Share/EnumExtentions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Share
+{
+    enum Role
+    {
+        Admin = 0,
+        Staff = 1
+    }
+}
